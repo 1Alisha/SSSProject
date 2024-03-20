78758307493a3220e318792a0258b0f9f86352f7
1Alisha <101705527+1Alisha@users.noreply.github.com>
2024-03-20 15:30:28 +0530

    ..

---
+++ mysrc.js
@@ -0,0 +1,84 @@
+document.write("my external JS file ");
+ 
+
+            var Addition="Alisha Gupta:: ";
+            document.write("<font size=5px><br><br><b>Addition===>");
+            var a,b,c;
+            a=60;
+            b=20;
+            c=a+b;
+            document.write(c);
+        
+            var firstname="MULTIPLY:: ";
+            document.write("<br><br><b>Multiplication===>");
+            var x,y,z;
+             x = 5;
+             y = 2;
+             z = x * y;
+            document.write(z);
+            
+            var Division="Alisha Gupta:: ";
+            document.write("<br><br><b>Division===>");
+            var x,y,z;
+            x = 5;
+            y = 2;
+            z = x / y;
+            document.write(z);
+
+            var Division="Alisha Gupta:: ";
+            document.write("<br><br><b>Modulus Operator===>");
+            var x,y,z;
+            x = 5;
+            y = 2;
+            z = x % y;
+            document.write(z);
+        
+            var Division="Alisha Gupta:: ";
+            document.write("<br><br><b>Subtraction===>");
+            var x,y,z;
+            x = 5;
+            y = 2;
+            z = x-y;
+            document.write(z);
+        
+            var firstname="MULTIPLY:: ";
+            document.write("<br><br><b>Exponential===>");
+            var x,y,z;
+             x = 5;
+             y = 2;
+             z = x ** y;
+            document.write(z);
+
+            var firstname="MULTIPLY:: ";
+            document.write("<br><br><b>Increment===>");
+            var x = 10;
+            x += 5;
+            document.write(x);
+
+            var firstname="MULTIPLY:: ";
+            document.write("<br><br><b>Contcatenation of strings===>");
+            var text1 = "Count your blessings ";
+            text1 += "not your problems.";
+            document.write(text1);
+
+            var firstname="MULTIPLY:: ";
+            document.write("<br><br><b>Decrement===>");
+            var x = 10;
+            x -= 5;
+            document.write(x);
+
+            var a = true, b = false;
+            var c = 4;
+            var linebreak = '\n';
+// logical AND 
+document.write(a && a); // true
+document.write(a && b);  // false 
+
+document.write((c > 2) && (c < 2)); // false
+var linebreak = '\n';
+var a = true, b = false, c = 4;
+// logical OR
+document.write(a || b); // true
+
+document.write(b || b); // false
+document.write((c>2) || (c<2)); // true
---
+++ site.js
@@ -0,0 +1,248 @@
+const data= [
+    {
+        id : 0,
+        img : 'https://static.cilory.com/550515-thickbox_default/multicolour-flared-printed-cotton-long-kurti.jpg',
+        name : 'Multicolor rayon kurti',
+        price : 190,
+        save : 25,
+        delievery : 'In 3 - 4 days',
+        itemInCart: false
+    },
+    
+        {
+            id : 1,
+            img : 'https://static.cilory.com/493595-thickbox_default/navy-blue-cotton-flared-printed-long-kurti.jpg',
+            name : 'Flared Printed Kurti',
+            price : 190,
+            save : 25,
+            delievery : 'In 3 - 4 days',
+            itemInCart: false
+        },
+
+        
+            {
+                id : 2,
+                img : 'https://static.cilory.com/550536-thickbox_default/mahendi-green-flared-printed-cotton-long-kurti.jpg',
+                name : 'Flared Printed Kurti',
+                price : 190,
+                save : 25,
+                delievery : 'In 3 - 4 days',
+                itemInCart: false
+            },
+            
+                {
+                    id : 3,
+                    img : 'https://static.cilory.com/550521-thickbox_default/brown-flared-embroidered-cotton-long-kurti.jpg',
+                    name : 'Flared Printed Kurti',
+                    price : 190,
+                    save : 25,
+                    delievery : 'In 3 - 4 days',
+                    itemInCart: false
+                },
+
+                
+                    {
+                        id : 4,
+                        img : 'https://static.cilory.com/550527-thickbox_default/grey-flared-embroidered-cotton-long-kurti.jpg',
+                        name : 'Flared Printed Kurti',
+                        price : 190,
+                        save : 25,
+                        delievery : 'In 3 - 4 days',
+                        itemInCart: false
+                    },
+
+
+                    
+                        {
+                            id : 5,
+                            img : 'https://static.cilory.com/566396-thickbox_default/flared-brown-cotton-silk-zari-beads-long-kurti.jpg',
+                            name : 'Flared Printed Kurti',
+                            price : 190,
+                            save : 25,
+                            delievery : 'In 3 - 4 days',
+                            itemInCart: false
+                        },
+ 
+
+                            {
+                                id : 6,
+                                img : 'https://sslimages.shoppersstop.com/sys-master/images/h02/hbf/17382684164126/AW21KSKU09FB_PURPLE.jpg_230Wx334H',
+                                name : 'Flared Printed Kurti',
+                                price : 190,
+                                save : 25,
+                                delievery : 'In 3 - 4 days',
+                                itemInCart: false
+                            },
+
+                         
+                                {
+                                    id : 7,
+                                    img : '',
+                                    name : 'Flared Printed Kurti',
+                                    price : 190,
+                                    save : 25,
+                                    delievery : 'In 3 - 4 days',
+                                    itemInCart: false
+                                },
+
+                              
+                                    {
+                                        id : 8,
+                                        img : 'https://sslimages.shoppersstop.com/sys-master/images/hf9/h9e/17498524057630/AW21KSKU501F_MAROON.jpg_230Wx334H',
+                                        name : 'Flared Printed Kurti',
+                                        price : 190,
+                                        save : 25,
+                                        delievery : 'In 3 - 4 days',
+                                        itemInCart: false
+                                    },
+
+
+                                 
+                                        {
+                                            id : 9,
+                                            img : 'https://sslimages.shoppersstop.com/sys-master/images/h8f/h4c/17498850525214/AW21KSKU204_TEAL.jpg_230Wx334H',
+                                            name : 'Flared Printed Kurti',
+                                            price : 190,
+                                            save : 25,
+                                            delievery : 'In 3 - 4 days',
+                                            itemInCart: false
+                                        },
+
+
+
+                                      
+                                            {
+                                                id : 10,
+                                                img : 'https://static.cilory.com/550540-thickbox_default/sky-blue-flared-embroidered-cotton-long-kurti.jpg',
+                                                name : 'Flared Printed Kurti',
+                                                price : 190,
+                                                save : 25,
+                                                delievery : 'In 3 - 4 days',
+                                                itemInCart: false
+                                            },
+
+
+
+                                         
+                                                {
+                                                    id : 11,
+                                                    img : 'https://static.cilory.com/489329-thickbox_default/cream-rayon-princess-line-printed-flared-kurti.jpg',
+                                                    name : 'Flared Printed Kurti',
+                                                    price : 190,
+                                                    save : 25,
+                                                    delievery : 'In 3 - 4 days',
+                                                    itemInCart: false
+                                                },
+
+
+
+
+
+                                             
+                                                    {
+                                                        id : 12,
+                                                        img : 'https://www.poojasarees.com/wp-content/uploads/2021/09/7.jpg',
+                                                        name : 'Flared Printed Kurti',
+                                                        price : 190,
+                                                        save : 25,
+                                                        delievery : 'In 3 - 4 days',
+                                                        itemInCart: false
+                                                    },
+
+
+
+                                                
+                                                        {
+                                                            id : 13,
+                                                            img : 'https://www.poojasarees.com/wp-content/uploads/2021/09/Blush-in-Pink.jpg',
+                                                            name : 'Flared Printed Kurti',
+                                                            price : 190,
+                                                            save : 25,
+                                                            delievery : 'In 3 - 4 days',
+                                                            itemInCart: false
+                                                        },
+
+
+
+
+
+
+
+
+                                                
+                                                            {
+                                                                id : 14,
+                                                                img : 'https://www.poojasarees.com/wp-content/uploads/2021/09/9.jpg',
+                                                                name : 'Flared Printed Kurti',
+                                                                price : 190,
+                                                                save : 25,
+                                                                delievery : 'In 3 - 4 days',
+                                                                itemInCart: false
+                                                            },
+
+                                                            {
+                                                                id : 15,
+                                                                img : 'https://static.cilory.com/489343-thickbox_default/multicolor-rayon-printed-flared-kurti.jpg',
+                                                                name : 'Flared printed kurti',
+                                                                price : 190,
+                                                                save : 25,
+                                                                delievery : 'In 3 - 4 days',
+                                                                itemInCart: false
+                                                            },
+
+                                                            {
+                                                                id : 16,
+                                                                img : 'https://static.cilory.com/493595-thickbox_default/navy-blue-cotton-flared-printed-long-kurti.jpg',
+                                                                name : 'Flared printed kurti',
+                                                                price : 190,
+                                                                save : 25,
+                                                                delievery : 'In 3 - 4 days',
+                                                                itemInCart: false
+                                                            },
+
+
+                                                            {
+                                                                id : 17,
+                                                                img : 'https://static.cilory.com/554893-thickbox_default/bell-sleeves-mauve-glow-cotton-embroidered-long-kurti.jpg',
+                                                                name : 'Flared printed kurti',
+                                                                price : 190,
+                                                                save : 25,
+                                                                delievery : 'In 3 - 4 days',
+                                                                itemInCart: false
+                                                            },
+
+                                                        ];
+
+                                                        let cartList=[]; //array to store cart lists         
+
+                                                        var i;
+                                                        var detail =document.getElementsByClassName('card box-shadow'); 
+                                                        var detailsImg = document.getElementById('detail-img')
+                                                        var detailTitle = document.getElementById('details-title')
+                                                        var detailPrice = document.getElementById('detail-price')
+                                                        var youSave = document.getElementById('you-save');
+                                                        var detailsPage = document.getElementById('container1');
+                                                        var back = document.getElementById('buy')
+                                                        back.addEventListener('click',refreshPage)
+                                                        var addToCarts = document.querySelectorAll('#add-to-cart')
+                                                        var cart = document.getElementById('cart');
+
+                                                        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
